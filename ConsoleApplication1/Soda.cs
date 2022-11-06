3188e8e3a539c6e9f305d5551c8e25b1e9ef82d3
Erik <enilsenhaga@gmail.com>
2022-11-06 19:30:49 +0100

    seperate files

---
+++ ConsoleApplication1/Soda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class Soda
+    {
+        public string Name { get; set; }
+        public int Nr { get; set; }
+
+    }
+}
