f68a1a613410fbca081d450c8736fbbb4a8ca9ec
Erik <enilsenhaga@gmail.com>
2022-11-07 10:40:26 +0100

    add happy past order tests

--- SodaMachineUnitTests/SodaMachineShould.cs
+++ SodaMachineUnitTests/SodaMachineShould.cs
@@ -2,23 +2,81 @@
 using ConsoleApplication1;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FluentAssertions;
+using ConsoleApplication1.Actions;
+using System.Linq;
 
 namespace SodaMachineUnitTests
 {
     [TestClass]
     public class SodaMachineShould
     {
-        SodaMachine sut = new SodaMachine();
+        Inventory cola1;
+        Inventory sprite1;
+        Inventory fanta1;
+        Inventory cola2;
+        InventoryCollection inv;
+        SodaMachine sut;
+
+        public SodaMachineShould()
+        {
+            cola1 = new Inventory(ReferenceData.Cola, 10);
+            sprite1 = new Inventory(ReferenceData.Sprite, 0);
+            fanta1 = new Inventory(ReferenceData.Fanta, 5);
+            cola2 = new Inventory(ReferenceData.Cola, 0);
+            inv = new InventoryCollection(cola1, sprite1, fanta1, cola2);
+            sut = new SodaMachine(inv);
+        }
 
         [TestMethod]
         public void OnInsert_UpdateBalance()
         {
             //Arrange
             //Act
-            sut.Insert(5);
-            sut.Insert(20);
+            var result1 = sut.Insert(5);
+            var result2 = sut.Insert(20);
             //Assert
-            sut.Balance.Should();
+            sut.Balance.Should().Be(25);
+            var firstResult=result1.Should().HaveCount(1).And.Subject.First();
+            firstResult.Should().BeOfType<NoOp>();
+            firstResult.Msg.Should().Be("Adding 5 to credit");
+            firstResult = result2.Should().HaveCount(1).And.Subject.First();
+            firstResult.Should().BeOfType<NoOp>();
+            firstResult.Msg.Should().Be("Adding 20 to credit");
+        }
+
+        [TestMethod]
+        public void OnOrder_UpdateBalanceInventoryAndEmitSoda()
+        {
+            //Arrange
+            int intialInsert = 40;
+            var remainder = intialInsert - ReferenceData.Cola.Price;
+            sut.Insert(intialInsert);
+            //Act
+            var result = sut.Order("coke").ToArray();
+            //Assert
+            sut.Balance.Should().Be(0);
+            result.Should().HaveCount(2);
+            result[0].Should().BeOfType<EmitSoda>();
+            result[0].Msg.Should().Be("Giving coke out");
+            result[1].Should().BeOfType<ReturnMoney>();
+            result[1].Msg.Should().Be($"Returning {remainder} to customer");
+            cola1.Amount.Should().Be(9);
+        }
+
+        [TestMethod]
+        public void OnSmsOrder_UpdateInventoryAndEmitSoda()
+        {
+            //Arrange
+            int intialInsert = 40;
+            sut.Insert(intialInsert);
+            //Act
+            var result = sut.SmsOrder("coke").ToArray();
+            //Assert
+            sut.Balance.Should().Be(40);
+            result.Should().HaveCount(1);
+            result[0].Should().BeOfType<EmitSoda>();
+            result[0].Msg.Should().Be("Giving coke out");
+            cola1.Amount.Should().Be(9);
         }
     }
 }
