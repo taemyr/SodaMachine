e96a9148587e85fc47da5a2fbcd01ce0b636de08
Erik <enilsenhaga@gmail.com>
2022-11-07 10:32:43 +0100

    cola->coke

--- ConsoleApplication1/ReferenceData.cs
+++ ConsoleApplication1/ReferenceData.cs
@@ -8,7 +8,7 @@ namespace ConsoleApplication1
     public class ReferenceData
     {
 #warning Todo:Remove Hardcoded values.
-        public static readonly Soda Cola = new Soda() { Name="cola", Price = 20 };
+        public static readonly Soda Cola = new Soda() { Name="coke", Price = 20 };
         public static readonly Soda Sprite = new Soda() { Name = "sprite", Price = 15 };
         public static readonly Soda Fanta = new Soda() { Name = "fanta", Price = 15 };
     }
