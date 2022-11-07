c26f95d1a3d14a0d76a5e2163d3bcedd4d5800a4
Erik <enilsenhaga@gmail.com>
2022-11-07 10:06:37 +0100

    create test project

---
+++ SodaMachineUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SodaMachineUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SodaMachineUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8971362e-5889-4dbc-94df-4f149ae6fe34")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SodaMachineUnitTests/SodaMachineShould.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ConsoleApplication1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace SodaMachineUnitTests
+{
+    [TestClass]
+    public class SodaMachineShould
+    {
+        SodaMachine sut = new SodaMachine();
+
+        [TestMethod]
+        public void OnInsert_UpdateBalance()
+        {
+            //Arrange
+            //Act
+            sut.Insert(5);
+            sut.Insert(20);
+            //Assert
+            sut.Balance.Should();
+        }
+    }
+}
